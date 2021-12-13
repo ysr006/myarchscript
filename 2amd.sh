baa30b1f2831cbcd9a246e36c5e7b527a3e2c183
ysr006 <84122323+ysr006@users.noreply.github.com>
2021-12-13 06:54:18 +0300

    Update 2amd.sh

--- 2amd.sh
+++ 2amd.sh
@@ -10,4 +10,4 @@ genfstab -U /mnt >> /mnt/etc/fstab
 pacstrap /mnt base base-devel linux linux-firmware amd-ucode networkmanager nano
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
-hwclock -systohc
+hwclock --systohc
