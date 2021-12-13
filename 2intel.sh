af40724f6bd620fe60bdd4d3f0bc69b9c8692f64
ysr006 <84122323+ysr006@users.noreply.github.com>
2021-12-13 06:56:39 +0300

    Update 2intel.sh

--- 2intel.sh
+++ 2intel.sh
@@ -1,6 +1,6 @@
 cd
 mkdir /mnt/boot
-mkfs.fat -F 32 /dev/sda/1
+mkfs.fat -F 32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
 swapon /dev/sda2
@@ -10,4 +10,4 @@ genfstab -U /mnt >> /mnt/etc/fstab
 pacstrap /mnt base base-devel linux linux-firmware intel-ucode networkmanager nano
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
-hwclock -systohc
+hwclock --systohc
