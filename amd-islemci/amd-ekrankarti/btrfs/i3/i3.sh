cd5caaa59b927ff7da626a68df29a8d55938f7fe
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-02-13 02:28:56 +0300

    Update i3.sh

--- amd-islemci/amd-ekrankarti/btrfs/i3/i3.sh
+++ amd-islemci/amd-ekrankarti/btrfs/i3/i3.sh
@@ -1,3 +1,18 @@
 cd
 loadkeys trq
 timedatectl set-ntp true
+cd
+mkdir -p /mnt/boot
+mkdir -p /mnt/boot/efi
+mkfs.fat -F 32 /dev/sda1
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
+swapon /dev/sda2
+mount /dev/sda3 /mnt
+mount /dev/sda1 /mnt/boot
+genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-firmware amd-ucode networkmanager nano sh
+cp -r myarchscript /mnt
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
+hwclock --systohc
