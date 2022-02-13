968de8562186b430622d0b0a0285db0bcf08f469
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-02-13 04:06:36 +0300

    Update arch.sh

--- amd-islemci/amd-ekrankarti/btrfs/hdd/swapon/i3/arch.sh
+++ amd-islemci/amd-ekrankarti/btrfs/hdd/swapon/i3/arch.sh
@@ -24,9 +24,11 @@ cp -r myarchscript /mnt
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
 hwclock --systohc
+echo "tr_TR.UTF-8 UTF-8" >> /etc/locale.gen
+echo "tr_TR ISO-8859-9" >> /etc/locale.gen
+locale-gen
 echo "LANG=tr_TR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=trq" >> /etc/vconsole.conf
-locale-gen
 echo "archlinux" >> /etc/hostname
 echo "127.0.0.1   localhost" >> /etc/hosts
 echo "::1         localhost" >> /etc/hosts
