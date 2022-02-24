f0efa2d6fbf4d4b6c9e8928c93faccf484fb8d20
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-02-24 21:37:13 +0300

    Update 1AA.sh

--- .Parçalar/1AA.sh
+++ .Parçalar/1AA.sh
@@ -7,14 +7,14 @@ mount /dev/sda2 /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@var
-mkdir -p /mnt/boot
-mkdir -p /mnt/boot/efi
-mkdir -p /mnt/home
-mkdir -p /mnt/var
 umount -R /mnt
 mount -o noatime,compress=zstd,subvol=@ /dev/sda2 /mnt
+mkdir -p /mnt/home
+mkdir -p /mnt/var
 mount -o noatime,compress=zstd,subvol=@home /dev/sda2 /mnt/home
 mount -o noatime,compress=zstd,subvol=@var /dev/sda2 /mnt/var
+mkdir -p /mnt/boot
+mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 genfstab -U /mnt >> /mnt/etc/fstab
 pacstrap /mnt base base-devel linux linux-headers linux-firmware networkmanager nano sh sudo
