d518d4609717ec2e34bc0f8ed0bdc15b710f7967
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-02-24 20:23:47 +0300

    Create 1AB.sh

---
+++ .Parçalar/1AB.sh
@@ -0,0 +1,22 @@
+cd
+loadkeys trq
+timedatectl set-ntp true
+mkfs.fat -F 32 /dev/sda1
+mkswap /dev/sda2
+mkfs.btrfs /dev/sda3
+mount /dev/sda3 /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var
+mkdir -p /mnt/boot
+mkdir -p /mnt/boot/efi
+mkdir -p /mnt/home
+mkdir -p /mnt/var
+umount -R /mnt
+mount -o noatime,compress=zstd,subvol=@ /dev/sda3 /mnt
+mount -o noatime,compress=zstd,subvol=@home /dev/sda3 /mnt/home
+mount -o noatime,compress=zstd,subvol=@var /dev/sda3 /mnt/var
+mount /dev/sda1 /mnt/boot/efi
+swapon /dev/sda2
+genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-headers linux-firmware networkmanager nano sh sudo
