489924af5319c835d56395c2d9c156cdd5db59be
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-02-25 19:36:20 +0300

    Create 1BB.sh

---
+++ .parts/1BB.sh
@@ -0,0 +1,13 @@
+cd
+loadkeys trq
+timedatectl set-ntp true
+mkfs.fat -F 32 /dev/sda1
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
+mount /dev/sda3 /mnt
+swapon /dev/sda2
+mkdir -p /mnt/boot
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-headers linux-firmware networkmanager nano sh sudo
