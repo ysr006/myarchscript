56fb50aa852ef3d69d3a48527650310e5c872016
ysr006 <84122323+ysr006@users.noreply.github.com>
2022-02-25 19:56:35 +0300

    Update arch.sh

--- amd-islemci/amd-ekrankarti/btrfs/hdd/swapoff/xfce4/arch.sh
+++ amd-islemci/amd-ekrankarti/btrfs/hdd/swapoff/xfce4/arch.sh
@@ -1,5 +1,5 @@
 cd
-sh myarchscript/.Parçalar/1AA.sh
-sh myarchscript/.Parçalar/2.sh
-sh myarchscript/.Parçalar/3A.sh
-sh myarchscript/.Parçalar/4A.sh
+sh myarchscript/.parts/1AA.sh
+sh myarchscript/.parts/2.sh
+sh myarchscript/.parts/3A.sh
+sh myarchscript/.parts/4A.sh
